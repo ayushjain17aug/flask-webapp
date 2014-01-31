2b1c1e2462805a0568e60998b43253b2291ea50e
Yura Shevchuk <yurayu92@gmail.com>
2014-01-31 19:25:07 +0200

    First commit

---
+++ app/views/base.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='../../templates',
+            static_folder='../../static', static_url_path='/static')
+
+
+@app.route('/', methods=('GET',))
+def home():
+    return render_template('home.html')
---
+++ run.py
@@ -0,0 +1,33 @@
+"""Web application
+
+Usage:
+    run.py server
+    run.py server <host>:<port>
+
+Options:
+    server - Run server. By default: localhost:5000
+
+"""
+from docopt import docopt
+
+from app.views.base import app
+from config import settings
+
+
+HOST = 'localhost:5000'
+
+
+def run_server(host=None):
+    if host is None:
+        host = HOST
+
+    host, port = host.split(':')
+
+    app.run(debug=settings.DEBUG, host=host, port=int(port))
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+
+    if arguments.get('server', None):
+        run_server(host=arguments.get('<host>:<port>', None))
