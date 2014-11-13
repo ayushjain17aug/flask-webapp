414fb45c60bfd1e21b0bee22944deb24670f0a80
Yura Shevchuk <mail@itdxer.com>
2014-11-13 16:28:17 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,8 @@
 DEBUG = True
 
 # Just for test program
-INSTAGRAM_CLIENT_ID = '658f4a3fa03c4737b4a8b170ffb8a0fe'
+INSTAGRAM_CLIENT_ID = '*****'
 INSTAGRAM_CLIENT_SECRET = '*****'  # Your key
 
-TUMBLR_OAUTH_KEY = 'Zs7KqurrByBMnGW5CKbg0dJJw0s2kyWYezPyQGqLhQGWwNko33'
+TUMBLR_OAUTH_KEY = '*****'
 TUMBLR_SECRET_KEY = '*****'  # Your key
